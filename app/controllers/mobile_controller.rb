55de3da05517e1c373ddc78c8e83215097b7339a
EC2 Default User <ec2-user@ip-172-31-1-86.ap-northeast-2.compute.internal>
2017-10-30 08:50:19 +0000

    첫번째 커밋

---
+++ app/controllers/mobile_controller.rb
@@ -0,0 +1,35 @@
+class MobileController < ApplicationController
+  def helloWorld
+    userAge = 10
+    height = 180.3
+    myJson = {"이름" => "조경진", "키" => height, "나이" => userAge, "메세지" => "HelloWorld!"}
+    render json: myJson
+  end
+
+  def sign_up
+
+    u = User.new
+    u.loginId = params[:id]
+    u.password = params[:pw]
+    u.name = params[:name]
+    u.save
+
+    myJson = {"result" => TRUE, "userInfo" => u}
+    render json: myJson
+
+  end
+
+  def sign_in
+    # 아이디와 비번이 올바른 사용자가 있는가?
+
+    if User.where(loginId: params[:id], password: params[:pw]).exists?
+      myJson = {"result" => TRUE, "메세지" => "로그인 성공"}
+      render json: myJson
+    else
+      myJson = {"result" => FALSE, "메세지" => "로그인 실패"}
+      render json: myJson
+    end
+
+  end
+
+end
---
+++ app/controllers/web_controller.rb
@@ -0,0 +1,7 @@
+class WebController < ApplicationController
+  def hello
+    name = "조경진"
+    @name2 = "조경진2"
+    @height = 190
+  end
+end
